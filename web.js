defecaef53b7ea524c3517fbf47a91a919534533
IaroslavBabenko <iaroslav.babenko@gmail.com>
2013-08-05 16:48:23 +0000

    Pretty up web.js

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-
-var outfile = "index.html";
-var text = fs.readFileSync(outfile);
+var htmlfile = "index.html"
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(text.toString());
+    var html = fs.readFileSync(htmlfile).toString(); 
+    response.send(text.toString());
 });
 
 var port = process.env.PORT || 8080;
